159b798af2029773d8828b9702d6d034720fd7b8
ZxchxryB <zack_berube@hotmail.ca>
2026-02-24 21:10:01 -0500

    Fixed World

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -8,27 +8,27 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var camera: Camera3D = $Camera3D
 
 func _ready():
-    add_to_group("player")
-    Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	add_to_group("player")
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _unhandled_input(event):
-    if event is InputEventMouseMotion:
-        rotate_y(-event.relative.x * mouse_sensitivity)
-        camera.rotate_x(-event.relative.y * mouse_sensitivity)
-        camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-80), deg_to_rad(80))
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		camera.rotate_x(-event.relative.y * mouse_sensitivity)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-80), deg_to_rad(80))
 
 func _physics_process(delta):
-    if not is_on_floor():
-        velocity.y -= gravity * delta
+	if not is_on_floor():
+		velocity.y -= gravity * delta
 
-    var input_dir = Input.get_vector("move_left","move_right","move_forward","move_back")
-    var direction = (transform.basis * Vector3(input_dir.x,0,input_dir.y)).normalized()
+	var input_dir = Input.get_vector("move_left","move_right","move_forward","move_back")
+	var direction = (transform.basis * Vector3(input_dir.x,0,input_dir.y)).normalized()
 
-    if direction:
-        velocity.x = direction.x * speed
-        velocity.z = direction.z * speed
-    else:
-        velocity.x = move_toward(velocity.x,0,speed)
-        velocity.z = move_toward(velocity.z,0,speed)
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x,0,speed)
+		velocity.z = move_toward(velocity.z,0,speed)
 
-    move_and_slide()
+	move_and_slide()
